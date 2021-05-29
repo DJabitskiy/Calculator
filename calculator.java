1d491e8b33ef1750e820e4f784b8da42dcc69d50
DJabitskiy <DJOlexandr@gmail.com>
2021-05-29 23:54:52 +0300

    going to normal calculator...sometimes...maybe

--- calculator.java
+++ calculator.java
@@ -1,29 +1,63 @@
 import javax.swing.*;
-import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.BorderLayout;
 
 public class calculator {
 
-   public static void main(String[] args) {
-      JPanel windowcontent = new JPanel();
-      FlowLayout fl = new FlowLayout();
-      windowcontent.setLayout(fl);
-      JLabel label1 = new JLabel("первое число");
-      JTextField field1 = new JTextField(10);
-      JLabel label2 = new JLabel("второе число");
-      JTextField field2 = new JTextField(10);
-      JLabel label3 = new JLabel("результат");
-      JTextField result = new JTextField(10);
-      JButton go = new JButton("сложить");
-      windowcontent.add(label1);
-      windowcontent.add(field1);
-      windowcontent.add(label2);
-      windowcontent.add(field2);
-      windowcontent.add(label3);
-      windowcontent.add(result);
-      windowcontent.add(go);
-      JFrame frame = new JFrame("mycalc");
-      frame.setContentPane(windowcontent);
-      frame.setSize(500, 100);
+   JPanel windowContent;
+   JTextField displayField;
+   JButton buttonPoint;
+   JButton buttonEqual;
+   JButton buttonplus;
+   JButton buttonminus;
+   JButton buttonmultiplication;
+   JButton buttondivision;
+   JPanel p1;
+   JPanel p2;
+
+   calculator() {
+      p1 = new JPanel();
+      p2 = new JPanel();
+      GridLayout gl = new GridLayout(4, 3);
+      p1.setLayout(gl);
+      p2.setLayout(gl);
+
+      int i, n = 9;
+      n++;
+      JButton[] buttons = new JButton[n];
+      n = n - 1;
+      for (i = 0; i <= n; i++) {
+         buttons[i] = new JButton("" + i);
+         p1.add(buttons[i]);
+      }
+      windowContent = new JPanel();
+      BorderLayout bl = new BorderLayout();
+      windowContent.setLayout(bl);
+      displayField = new JTextField(30);
+      windowContent.add("North", displayField);
+      buttonPoint = new JButton(".");
+      buttonEqual = new JButton("=");
+      p1.add(buttonPoint);
+      p1.add(buttonEqual);
+      buttonplus = new JButton("+");
+      p2.add(buttonplus);
+      buttonminus = new JButton("-");
+      p2.add(buttonminus);
+      buttondivision = new JButton("/");
+      p2.add(buttondivision);
+      buttonmultiplication = new JButton("*");
+      p2.add(buttonmultiplication);
+      windowContent.add("East", p2);
+      windowContent.add("Center", p1);
+
+      JFrame frame = new JFrame("Calculator");
+      frame.setContentPane(windowContent);
+      frame.pack();
       frame.setVisible(true);
+   }
+
+   public static void main(String[] args) {
+      calculator calc = new calculator();
+
    }
 }
