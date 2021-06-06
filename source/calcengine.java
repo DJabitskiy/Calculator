ab929bb4a16d40aa21ce16cce852f970ee3fbecb
DJabitskiy <DJOlexandr@gmail.com>
2021-06-06 22:21:15 +0300

    work mostly

--- source/calcengine.java
+++ source/calcengine.java
@@ -7,14 +7,54 @@ import javax.swing.JButton;
 public class calcengine implements ActionListener {
    calculator parent;
 
+   char action = ' ';
+   double result = 0;
+
    calcengine(calculator parent) {
       this.parent = parent;
    }
 
    public void actionPerformed(ActionEvent e) {
       JButton clickedButton = (JButton) e.getSource();
-      String display = parent.displayField.getText();
-      String clickedButtonLabel = clickedButton.getText();
-      parent.displayField.setText(display + clickedButtonLabel);
+      String dispFieldText = parent.displayField.getText();
+      double displayValue = 0;
+      if (!"".equals(dispFieldText)) {
+         displayValue = Double.parseDouble(dispFieldText);
+      }
+      Object src = e.getSource();
+      if (src == parent.buttonplus) {
+         action = '+';
+         result = displayValue;
+         parent.displayField.setText("");
+      } else if (src == parent.buttonminus) {
+         action = '-';
+         result = displayValue;
+         parent.displayField.setText("");
+      } else if (src == parent.buttondivision) {
+         action = '/';
+         result = displayValue;
+         parent.displayField.setText("");
+      } else if (src == parent.buttonmultiplication) {
+         action = '*';
+         result = displayValue;
+         parent.displayField.setText("");
+      } else if (src == parent.buttonEqual) {
+         if (action == '+') {
+            result += displayValue;
+            parent.displayField.setText("" + result);
+         } else if (action == '-') {
+            result -= displayValue;
+            parent.displayField.setText("" + result);
+         } else if (action == '/') {
+            result /= displayValue;
+            parent.displayField.setText("" + result);
+         } else if (action == '*') {
+            result *= displayValue;
+            parent.displayField.setText("" + result);
+         }
+      } else {
+         String clickedButtonLabel = clickedButton.getText();
+         parent.displayField.setText(dispFieldText + clickedButtonLabel);
+      }
    }
 }
